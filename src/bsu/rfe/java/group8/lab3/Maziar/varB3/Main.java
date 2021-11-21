d6184d6a36aedc6a865c418cbc3dd0a7023212a3
Dima Maziar <dzmaziar@mail.ru>
2021-11-21 17:52:55 +0300

    ADDED MainFrame and Main

--- src/bsu/rfe/java/group8/lab3/Maziar/varB3/Main.java
+++ src/bsu/rfe/java/group8/lab3/Maziar/varB3/Main.java
@@ -1,8 +1,12 @@
 package bsu.rfe.java.group8.lab3.Maziar.varB3;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	MainFrame frame = new MainFrame();
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.setVisible(true);
     }
 }
